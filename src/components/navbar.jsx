b215631524f475ece6e63d6c133e96719af7af9b
pratham rathore <prathamrathore2004@gmail.com>
2025-04-23 11:55:41 +0530

    initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-15 bg-gradient-to-r from-green-400 to-blue-500 flex justify-center items-center font-bold text-3xl text-white">
+      <h1>Your To-Do-list</h1>
+    </div>
+  );
+};
+
+export default Navbar;
